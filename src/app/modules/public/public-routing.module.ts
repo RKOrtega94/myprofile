dd2fe3dc739cd7eb6a4042975a4d021f3e1c32b0
RKOrtega94 <rkortega1994@gmail.com>
2022-03-06 13:51:05 -0500

    refactor Sidenav

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -6,10 +6,12 @@ import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/shared/componens/sidenav/sidenav.component.ts
+++ src/app/shared/componens/sidenav/sidenav.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  sidenavList = [
+    { path: '/home', title: 'Home', icon: 'bx-home' },
+    { path: '/about', title: 'About', icon: 'bx-user' },
+    { path: '/resume', title: 'Resume', icon: 'bx-file-blank' },
+    { path: '/portfolio', title: 'Portfolio', icon: 'bx-book-content' },
+    { path: '/services', title: 'Services', icon: 'bx-server' },
+    { path: '/contact', title: 'Contact', icon: 'bx-envelope' },
+  ];
+  routeName: string;
+  constructor(private router: Router) {
+    this.routeName = this.router.url;
   }
 
+  ngOnInit(): void {}
 }
