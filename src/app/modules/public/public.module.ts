7c0ee8264a4d3c71f9638c6084c862705648e525
RKOrtega94 <rkortega1994@gmail.com>
2022-03-06 11:43:19 -0500

    Home page completed

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { LayoutComponent } from './layout/layout.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
-  declarations: [HomeComponent, LayoutComponent],
-  imports: [CommonModule, PublicRoutingModule, SharedModule],
+  declarations: [HomeComponent],
+  imports: [CommonModule, PublicRoutingModule, SharedModule, NgxTypedJsModule],
 })
 export class PublicModule {}
