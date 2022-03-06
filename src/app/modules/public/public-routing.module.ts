f61c8a3dd453f99d2188372c4934884869dfdefb
RKOrtega94 <rkortega1994@gmail.com>
2022-03-06 12:09:14 -0500

    Refactors

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -5,9 +5,10 @@ import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [CommonModule, PublicRoutingModule, SharedModule, NgxTypedJsModule],
 })
 export class PublicModule {}
