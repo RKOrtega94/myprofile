1847ca5b7c72628fc7a3756fd5a3d8b5e7af8b67
RKOrtega94 <rkortega1994@gmail.com>
2022-03-06 12:10:27 -0500

    Add resume component

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 import { AboutComponent } from './pages/about/about.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, ResumeComponent],
   imports: [CommonModule, PublicRoutingModule, SharedModule, NgxTypedJsModule],
 })
 export class PublicModule {}
