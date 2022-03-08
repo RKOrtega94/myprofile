c334a9d222d56914e889d417a2cfbd9766ae6ca4
RKOrtega94 <rkortega1994@gmail.com>
2022-03-08 17:55:11 -0500

    Refactor

--- src/app/shared/componens/sidenav/sidenav.component.ts
+++ src/app/shared/componens/sidenav/sidenav.component.ts
@@ -14,8 +14,8 @@ export class SidenavComponent implements OnInit {
     { path: '/resume', title: 'Resume', icon: 'bx-file-blank' },
     { path: '/portfolio', title: 'Portfolio', icon: 'bx-book-content' },
     /* { path: '/photography', title: 'Photography', icon: 'bx-images' }, */
-    { path: '/services', title: 'Services', icon: 'bx-server' },
-    { path: '/contact', title: 'Contact', icon: 'bx-envelope' },
+    /* { path: '/services', title: 'Services', icon: 'bx-server' },
+    { path: '/contact', title: 'Contact', icon: 'bx-envelope' }, */
   ];
   routeName: string;
   isOpen: boolean = false;
